ca9c5b52ec9e1361e54e0c3158ffa525d7e1c13c
Sidra Saleem <79654692+SidraSaleem296@users.noreply.github.com>
2024-11-30 21:19:09 +0500

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        return f"<h1>You entered: {user_input}</h1>"
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
