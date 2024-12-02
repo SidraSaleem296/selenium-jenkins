e49c9a506e157fe72e88da327166937d8ead073e
Sidra Saleem <79654692+SidraSaleem296@users.noreply.github.com>
2024-12-02 23:15:49 +0500

    Update test_app.py\

--- test_app.py
+++ test_app.py
@@ -1,23 +1,18 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-class WebAppTest(unittest.TestCase):
+def test_title():
+    driver = webdriver.Chrome()
+    driver.get("http://44.244.121.50:5000")
+    assert "Simple Web App" in driver.title
+    driver.quit()
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-
-    def test_submit_form(self):
-        driver = self.driver
-        driver.get("http://44.243.227.79:5000")
-        input_box = driver.find_element(By.NAME, "user_input")
-        input_box.send_keys("Test Input")
-        input_box.send_keys(Keys.RETURN)
-        self.assertIn("You entered: Test Input", driver.page_source)
-
-    def tearDown(self):
-        self.driver.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_form_submission():
+    driver = webdriver.Chrome()
+    driver.get("http://44.244.121.50:5000")
+    input_box = driver.find_element(By.NAME, "user_input")
+    input_box.send_keys("Test Input")
+    input_box.send_keys(Keys.RETURN)
+    assert "You entered: Test Input" in driver.page_source
+    driver.quit()
